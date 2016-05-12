b2a645ad52f6c03401dfba5782bdb41ace99b9a6
carloare <carlo.are@tiscali.it>
2016-05-12 20:23:22 +0200

    reset

--- SardiniaInFood/php/view/out/footer_home_page.php
+++
@@ -1,8 +0,0 @@
- <ul class="menu">
-     <li>
-        <a href="#">Login Azienda</a>
-    </li>
-     <li>
-         <a href="#">Aggiungi la tua Azienda</a>
-     </li>
- </ul>  
--- SardiniaInFood/php/view/out/menu_home_page.php
+++
@@ -1,8 +0,0 @@
-<ul class="menu">
-    <li>
-        <a href="#">Accedi</a>
-    </li>
-    <li>
-         <a href="#">Iscriviti</a>
-     </li>
-</ul>
