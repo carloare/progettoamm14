7c129b30f047ba50fb048a173e675988ecc74035
carloare <carlo.are@tiscali.it>
2016-06-17 18:09:47 +0200

    fix

--- SardiniaInFood/php/view/out/home_page_default.php
+++ SardiniaInFood/php/view/out/home_page_default.php
@@ -63,11 +63,11 @@
    
     
     
-    include_once basename(__DIR__) . '/../Utente.php';
+    include_once '/../Utente.php';
     
-    include_once basename(__DIR__) . '/../UtenteFactory.php';
+    include_once '/../UtenteFactory.php';
 
-    include_once basename(__DIR__) . '/../Azienda.php';
+    include_once '/../Azienda.php';
    
    
 
