d367216f63bb87bafdebda650ac4c062e111c245
carloare <carlo.are@tiscali.it>
2016-06-29 19:26:44 +0200

    fix vari

--- SardiniaInFood/php/view/in/menu_cliente.php
+++ SardiniaInFood/php/view/in/menu_cliente.php
@@ -1,16 +1,78 @@
 <ul class="menu">
+<?php
 
-
-<div id="esc">
-     <li>     
-             <a href="?cmd=back_home_page" method="POST">Home</a>
-        </li>
+if($_REQUEST['cmd']=='show_preferiti' || $_REQUEST['cmd']=='indietro')
+{
+    
+echo '
     <li>
-  <!--bottone per il logout-->
+        <li>     
+             <a href="?cmd=back_home_page" title="ritorna alla Homepage" method="POST">Home</a>
+        </li>
+    </li>
+';
+}
+
+
+
+if($_REQUEST['cmd']=='cercadovecosa')
+{
+    
+echo '
+    <li>
+        <li>     
+             <a href="?cmd=back_home_page" title="pulisci dai risultati" method="POST">Pulisci</a>
+        </li>
+    </li>
+';
+}
+
+if($_REQUEST['cmd']=='profileandvote')
+{
+echo '
+    <li>
+        <li>     
+             <a href="?cmd=indietro" method="POST" title="ritorna alla pagina precedente">&laquo; Indietro</a>
+        </li>
+    </li>
+';
+}
+
+else
+{
+    
+}
+
+
+if($_REQUEST['cmd']!='profileandvote' AND $_REQUEST['cmd']!='show_preferiti')
+{
+echo '
+     <li>
+        <li>     
+             <a href="/SardiniaInFood/php/controller/ClienteController.php?cmd=show_preferiti" title="mostra i miei preferiti" method="POST">Preferiti</a>
+        </li>
+    </li>
+';
+}
+else
+{
+    
+}
+
+?>
+
+
+    <div id="uscita">
+       <li>
+        <a class="alert" href ="/SardiniaInFood/php/controller/ClienteController.php?cmd=cancella" title="cancella il mio profilo" onclick ="return confirm('Sei sicuro?');">Cancella profilo</a>
+    </li>
+    <li>
+ 
         <a href="http://localhost/SardiniaInFood/php/index.php?page=0&logout" method="POST" title="Ritorna alla home page">Logout</a>
 
+        
     </li>
-   
-</div>
+    </div>
+    
     
 </ul>
