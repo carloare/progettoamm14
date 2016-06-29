52caa15f6d8dcbc4c188627a7f77a0f32031df27
carloare <carlo.are@tiscali.it>
2016-06-29 19:21:32 +0200

    update db

--- SardiniaInFood/amm14_aresuCarlo.sql
+++ SardiniaInFood/amm14_aresuCarlo.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generato il: Mar 12, 2016 alle 16:58
+-- Generato il: Apr 03, 2016 alle 19:22
 -- Versione del server: 5.5.35
 -- Versione PHP: 5.4.6-1ubuntu1.7
 
@@ -95,7 +95,16 @@ CREATE TABLE IF NOT EXISTS `Aziende` (
   PRIMARY KEY (`id`),
   KEY `tipo_incarichi_id` (`tipo_incarichi_id`),
   KEY `tipo_attivita_id` (`tipo_attivita_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Dump dei dati per la tabella `Aziende`
+--
+
+INSERT INTO `Aziende` (`id`, `nome_completo`, `tipo_incarichi_id`, `email_conferma`, `username`, `password`, `nome_azienda`, `citta`, `indirizzo`, `tipo_attivita_id`, `descrizione`, `telefono`, `email`, `sito_web`, `ruolo`) VALUES
+(1, 'Gigi', 1, 'gigi@email.it', 'gigi', '111', 'pear', 'miami', 'ocean drive', 17, 'descrizione di pear', '000000000', 'pear@email.it', 'www.pear.it', 1),
+(2, 'Franco', 1, 'franco@email.it', 'franco', '111', 'book', 'miami', '5th street', 17, 'descrizione book', '111111111', 'book@email.it', 'www.book.it', 1),
+(3, 'Piera', 1, 'piera@email.it', 'piera', '111', 'table', 'miami', '10th street', 17, 'descrizione table', '22222222', 'table@email.it', 'www.table.it', 1);
 
 -- --------------------------------------------------------
 
@@ -107,10 +116,21 @@ CREATE TABLE IF NOT EXISTS `Aziende_Servizi` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `id_aziende` bigint(20) unsigned DEFAULT NULL,
   `id_servizi` bigint(20) unsigned DEFAULT NULL,
+  `valore` int(1) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `id_aziende` (`id_aziende`),
   KEY `id_servizi` (`id_servizi`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+--
+-- Dump dei dati per la tabella `Aziende_Servizi`
+--
+
+INSERT INTO `Aziende_Servizi` (`id`, `id_aziende`, `id_servizi`, `valore`) VALUES
+(1, 1, 1, 1),
+(2, 1, 2, 1),
+(3, 3, 1, 0),
+(4, 3, 2, 0);
 
 -- --------------------------------------------------------
 
@@ -127,7 +147,14 @@ CREATE TABLE IF NOT EXISTS `Clienti` (
   `ruolo` int(1) DEFAULT NULL,
   `numero_richiami` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dump dei dati per la tabella `Clienti`
+--
+
+INSERT INTO `Clienti` (`id`, `nome_completo`, `username`, `password`, `email_conferma`, `ruolo`, `numero_richiami`) VALUES
+(2, 'gigi', 'gigi', '111', 'gigi@email.it', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -164,7 +191,14 @@ CREATE TABLE IF NOT EXISTS `Preferiti` (
   PRIMARY KEY (`id`),
   KEY `id_aziende` (`id_aziende`),
   KEY `id_clienti` (`id_clienti`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dump dei dati per la tabella `Preferiti`
+--
+
+INSERT INTO `Preferiti` (`id`, `id_aziende`, `id_clienti`) VALUES
+(1, 1, 2);
 
 -- --------------------------------------------------------
 
@@ -198,7 +232,21 @@ CREATE TABLE IF NOT EXISTS `Recensioni` (
   PRIMARY KEY (`id`),
   KEY `id_aziende` (`id_aziende`),
   KEY `id_clienti` (`id_clienti`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
+
+--
+-- Dump dei dati per la tabella `Recensioni`
+--
+
+INSERT INTO `Recensioni` (`id`, `id_aziende`, `id_clienti`, `data`, `recensione`, `numero_segnalazioni`) VALUES
+(1, 2, 2, '01/04/2016 - 17:32', 'lakjdfalkdjalkdjfalkjflakf', 0),
+(2, 3, 2, '01/04/2016 - 17:54', 'questa recensione Ã¨ stata inserita correttamente', 0),
+(3, 1, 2, '01/04/2016 - 19:17', 'test nuovo inserimento', 0),
+(4, 3, 2, '02/04/2016 - 15:54', 'questo Ã¨ un altro test spero vada a buon fine', 1),
+(5, 3, 2, '02/04/2016 - 17:59', 'test inserimento ulteriore commento', 0),
+(6, 3, 2, '03/04/2016', 'questo Ã¨ un altro test sperando che tutto vada bene', 0),
+(7, 3, 2, '03/04/2016', 'COMMENTO 5', 1),
+(8, 3, 2, '03/04/2016', 'COMMENTO 6', 2);
 
 -- --------------------------------------------------------
 
@@ -211,7 +259,17 @@ CREATE TABLE IF NOT EXISTS `Segnalazioni` (
   `id_recensioni` bigint(20) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `id_recensioni` (`id_recensioni`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15 ;
+
+--
+-- Dump dei dati per la tabella `Segnalazioni`
+--
+
+INSERT INTO `Segnalazioni` (`id`, `id_recensioni`) VALUES
+(14, 4),
+(11, 7),
+(12, 8),
+(13, 8);
 
 -- --------------------------------------------------------
 
@@ -260,11 +318,22 @@ CREATE TABLE IF NOT EXISTS `Statistiche` (
   `id_aziende` bigint(20) unsigned DEFAULT NULL,
   `visualizzazioni` int(11) DEFAULT NULL,
   `media_voto` float DEFAULT NULL,
-  `rapporto_qualita_prezzo` float DEFAULT NULL,
+  `numero_voti` int(11) NOT NULL,
+  `media_rapporto_qualita_prezzo` float DEFAULT NULL,
+  `numero_voti_qualita_prezzo` int(11) NOT NULL,
   `numero_preferenze` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `id_aziende` (`id_aziende`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Dump dei dati per la tabella `Statistiche`
+--
+
+INSERT INTO `Statistiche` (`id`, `id_aziende`, `visualizzazioni`, `media_voto`, `numero_voti`, `media_rapporto_qualita_prezzo`, `numero_voti_qualita_prezzo`, `numero_preferenze`) VALUES
+(1, 1, 27, 5, 1, 0, 0, 1),
+(2, 2, 14, 0, 0, 0, 0, 0),
+(3, 3, 95, 0, 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -280,7 +349,7 @@ CREATE TABLE IF NOT EXISTS `Voti` (
   PRIMARY KEY (`id`),
   KEY `id_clienti` (`id_clienti`),
   KEY `id_aziende` (`id_aziende`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
 -- Limiti per le tabelle scaricate
