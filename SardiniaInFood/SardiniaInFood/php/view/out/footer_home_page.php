d80e96d4dc83217fa02f275d96d773cfb85814d3
carloare <carlo.are@tiscali.it>
2016-07-22 09:11:58 +0200

    Delete footer_home_page.php

--- SardiniaInFood/SardiniaInFood/php/view/out/footer_home_page.php
+++
@@ -1,8 +0,0 @@
- <ul class="menu">
-     <li><!--richiama il login dell'azienda-->
-        <a href="/SardiniaInFood/php/?page=4" title="Login Aziende">Login Azienda</a>
-    </li>
-     <li><!--richiama la registrazione di una nuova azienda-->
-         <a href="/SardiniaInFood/php/?page=3" title="Registra la tua azienda">Aggiungi la tua Azienda</a>
-     </li>
- </ul>  
