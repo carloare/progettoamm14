0cecce2e0ee48177c41864d0ccaad939d1f5ded8
carloare <carlo.are@tiscali.it>
2016-07-22 09:12:56 +0200

    Delete menu_home_page.php

--- SardiniaInFood/SardiniaInFood/php/view/out/menu_home_page.php
+++
@@ -1,9 +0,0 @@
-
-<ul id="rec_login">
-    <li> <!--richiama la pagina del login per il cliente-->
-        <a href="/SardiniaInFood/php/?page=2" title="Login clienti">Accedi</a>
-    </li>
-    <li><!--richiama la pagina di registrazione per un nuovo cliente-->
-         <a href="/SardiniaInFood/php/?page=1" class="iscriviti" title="Iscriviti">Iscriviti</a>
-     </li>
-</ul>
