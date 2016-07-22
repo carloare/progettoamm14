c1e3bb90b6458ee690a17359faf8b89cf48fc736
carloare <carlo.are@tiscali.it>
2016-07-22 09:12:38 +0200

    Delete login_cliente.php

--- SardiniaInFood/SardiniaInFood/php/view/out/login_cliente.php
+++
@@ -1,9 +0,0 @@
-<form name="login_form" action="/SardiniaInFood/php/controller/BaseController.php" method="POST"> 
-    
- <p> Username: </p> <input type="text" name="username_cliente" value="" title="inserisci il tuo username"> 
-  
- <p> Password: </p> <input type="password" name="password_cliente" value="" title="inserisci la tua password"><br>
- 
-  <input type="hidden" name="cmd" value="login_cliente">
- <p> <input type="submit" id="submitlogin" name="submit" value="Login"> </p>
-</form> 
