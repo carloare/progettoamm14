9dbbf0ccbd19375244b04a8361f4eb7d092a63da
carloare <carlo.are@tiscali.it>
2016-08-05 21:27:36 +0200

    Update Settings.php

--- SardiniaInFood/php/Settings.php
+++ SardiniaInFood/php/Settings.php
@@ -2,37 +2,6 @@
 
 //Classe con le credenziali per l'accesso al database
 
-
-$conn = mysqli_connect("localhost", "aresuCarlo", "falco808", "amm14_aresuCarlo");
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
-/* check connection */
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-} else echo "ciao";
-
-
-$sql = "CREATE TABLE IF NOT EXISTS `Servizi` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `tipo` varchar(32) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8";
-
-if (mysqli_query($conn, $sql)) {
-    echo "Table Servizi created successfully";
-} else {
-    echo "Error creating table: " . mysqli_error($conn);
-}
-
-    
-if ($result = mysqli_query($conn, "SELECT * FROM Servizi")) {
-    $number_servizi = $result->num_rows;
-    echo "Numero servizi: ".$number_servizi;
-}
 class Settings
 {
     public static $db_host = 'localhost';
