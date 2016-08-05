b62103b2bb1dda105ae64d20e539993df4ec703d
carloare <carlo.are@tiscali.it>
2016-08-05 19:52:36 +0200

    Update menu_home_page.php

--- SardiniaInFood/php/view/out/menu_home_page.php
+++ SardiniaInFood/php/view/out/menu_home_page.php
@@ -1,10 +1,10 @@
 
     <li class="selected"> <!--richiama la home page-->
-        <a href="/SardiniaInFood/php/?page=0&logout" title="Home page">HOME</a>
+        <a href="http://spano.sc.unica.it/amm2014/aresuCarlo/SardiniaInFood/php/?page=0&logout" title="Home page">HOME</a>
     </li>
     <li class="user"> <!--richiama la pagina del login per il cliente-->
-        <a href="/SardiniaInFood/php/?page=2" title="Login clienti">LOGIN</a>
+        <a href="http://spano.sc.unica.it/amm2014/aresuCarlo/SardiniaInFood/php/?page=2" title="Login clienti">LOGIN</a>
     </li>
     <li class="user"><!--richiama la pagina di registrazione per un nuovo cliente-->
-         <a href="/SardiniaInFood/php/?page=1" title="Iscriviti">ISCRIVITI</a>
+         <a href="http://spano.sc.unica.it/amm2014/aresuCarlo/SardiniaInFood/php/?page=1" title="Iscriviti">ISCRIVITI</a>
      </li>
