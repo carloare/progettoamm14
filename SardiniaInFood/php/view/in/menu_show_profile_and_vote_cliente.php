81c2b663595180263d4dbe2e4a2fbca02e66ecff
carloare <carlo.are@tiscali.it>
2016-08-06 16:48:21 +0200

    Update menu_show_profile_and_vote_cliente.php

--- SardiniaInFood/php/view/in/menu_show_profile_and_vote_cliente.php
+++ SardiniaInFood/php/view/in/menu_show_profile_and_vote_cliente.php
@@ -1,11 +1,11 @@
 <div id="menu_show_profile_and_vote_cliente">
     <ul>
         <li>     
-             <a href="?cmd=indietro" method="POST" title="ritorna alla pagina precedente">&lt; Indietro</a>
+             <a href="http://spano.sc.unica.it/amm2014/aresuCarlo/SardiniaInFood/php/controller/ClienteController.php?cmd=indietro" method="POST" title="ritorna alla pagina precedente">&lt; Indietro</a>
         </li>
         <li>
  
-        <a href="http://localhost/SardiniaInFood/php/index.php?page=0&logout" method="POST" title="Esci">Logout</a>
+        <a href="http://spano.sc.unica.it/amm2014/aresuCarlo/SardiniaInFood/php/index.php?page=0&logout" method="POST" title="Esci">Logout</a>
 
         
     </li>
