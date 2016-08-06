b977501b1c759244733fb0bce0524f53f54ace44
carloare <carlo.are@tiscali.it>
2016-08-06 12:31:00 +0200

    Update footer_home_page.php

--- SardiniaInFood/php/view/out/footer_home_page.php
+++ SardiniaInFood/php/view/out/footer_home_page.php
@@ -1,8 +1,8 @@
  <ul class="main-menu-footer">
      <li><!--richiama il login dell'azienda-->
-        <a href="/SardiniaInFood/php/?page=4" title="Login Aziende">LOGIN AZIENDA</a>
+        <a href="http://spano.sc.unica.it/amm2014/aresuCarlo/SardiniaInFood/php/?page=4" title="Login Aziende">LOGIN AZIENDA</a>
     </li>
      <li><!--richiama la registrazione di una nuova azienda-->
-         <a href="/SardiniaInFood/php/?page=3" title="Registra la tua azienda">AGGIUNGI LA TUA AZIENDA</a>
+         <a href="http://spano.sc.unica.it/amm2014/aresuCarlo/SardiniaInFood/php/?page=3" title="Registra la tua azienda">AGGIUNGI LA TUA AZIENDA</a>
      </li>
  </ul>  
