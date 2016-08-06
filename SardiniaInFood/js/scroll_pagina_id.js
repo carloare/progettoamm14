12f64d4184baba51a3091222790996adb0456a41
carloare <carlo.are@tiscali.it>
2016-08-06 17:34:34 +0200

    Delete scroll_pagina_id.js

--- SardiniaInFood/js/scroll_pagina_id.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function($) {
-    var id = $('input:hidden').val();
-    //$('.favorites').attr('id');
-    alert(id);
-    //dopo l'inserimento di un commento viene fatto uno scrollo che porta nella parte superiore di 'top_recensioni'
-    $(window).scrollTop($("id").offset().top);
-});
