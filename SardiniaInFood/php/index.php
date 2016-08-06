f3aaa76fbfe7b3d375a891fe95dc2d9765f39f37
carloare <carlo.are@tiscali.it>
2016-08-06 15:55:35 +0200

    Update index.php

--- SardiniaInFood/php/index.php
+++ SardiniaInFood/php/index.php
@@ -5,10 +5,10 @@
 */
 
 
-   include_once 'view/ViewDescriptor.php';
-   include_once 'model/Utente.php';
-   include_once 'model/UtenteFactory.php';
-   include_once 'model/Azienda.php';
+   include_once '/home/amm/repoAmm/amm2014/aresuCarlo/SardiniaInFood/php/view/ViewDescriptor.php';
+   include_once '/home/amm/repoAmm/amm2014/aresuCarlo/SardiniaInFood/php/model/Utente.php';
+   include_once '/home/amm/repoAmm/amm2014/aresuCarlo/SardiniaInFood/php/model/UtenteFactory.php';
+   include_once '/home/amm/repoAmm/amm2014/aresuCarlo/SardiniaInFood/php/model/Azienda.php';
    
    if (session_status() != 2) session_start();
 
