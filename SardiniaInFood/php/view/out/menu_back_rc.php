5932b201644217639593c8de6cde4eccc7134f50
carloare <carlo.are@tiscali.it>
2016-08-06 17:36:29 +0200

    Delete menu_back_rc.php

--- SardiniaInFood/php/view/out/menu_back_rc.php
+++
@@ -1,8 +0,0 @@
-<div id="menu_back_rc">
-<ul>
-    <li><!--richiama la home page di default partendo dal form di registrazione del cliente-->
-        <a href="http://spano.sc.unica.it/amm2014/aresuCarlo/SardiniaInFood/php/?page=0" title="Torna indietro alla home page"> HOME PAGE</a>
-    </li>
-</ul>
-</div>
-
