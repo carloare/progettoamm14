da09ea3b4d2448e9491951ddec90735a8432eaff
carloare <carlo.are@tiscali.it>
2016-08-06 16:47:32 +0200

    Update menu_mostra_profilo_e_vota_cliente.php

--- SardiniaInFood/php/view/in/menu_mostra_profilo_e_vota_cliente.php
+++ SardiniaInFood/php/view/in/menu_mostra_profilo_e_vota_cliente.php
@@ -1,5 +1,5 @@
 
    <li>     
-      <a href="/SardiniaInFood/php/controller/ClienteController.php?cmd=indietro" method="POST" title="torna alla ricerca">HOME</a>
+      <a href="http://spano.sc.unica.it/amm2014/aresuCarlo/SardiniaInFood/php/controller/ClienteController.php?cmd=indietro" method="POST" title="torna alla ricerca">HOME</a>
    </li>
 
