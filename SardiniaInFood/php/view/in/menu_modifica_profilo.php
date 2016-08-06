5b03ac818cf07e2556eb08763fed9b585adca8ad
carloare <carlo.are@tiscali.it>
2016-08-06 16:47:06 +0200

    Update menu_modifica_profilo.php

--- SardiniaInFood/php/view/in/menu_modifica_profilo.php
+++ SardiniaInFood/php/view/in/menu_modifica_profilo.php
@@ -1,4 +1,4 @@
 
      <li>
-         <a href ="/SardiniaInFood/php/controller/AziendaController.php?cmd=back_home_page" title="torna alla tua home page">HOME</a>
+         <a href ="http://spano.sc.unica.it/amm2014/aresuCarlo/SardiniaInFood/php/controller/AziendaController.php?cmd=back_home_page" title="torna alla tua home page">HOME</a>
     </li>  
