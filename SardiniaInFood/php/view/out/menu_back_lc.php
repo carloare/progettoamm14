183edf26c8989a2c2bd74704ab557d631cdeb45b
carloare <carlo.are@tiscali.it>
2016-08-06 16:29:53 +0200

    Update menu_back_lc.php

--- SardiniaInFood/php/view/out/menu_back_lc.php
+++ SardiniaInFood/php/view/out/menu_back_lc.php
@@ -1,7 +1,7 @@
 <div id="menu_back_lc">
 <ul>
     <li><!--richiama la home page di default-->
-        <a href="/SardiniaInFood/php/?page=0" title="Torna indietro alla home page"> HOME PAGE</a>
+        <a href="http://spano.sc.unica.it/amm2014/aresuCarlo/SardiniaInFood/php/?page=0" title="Torna indietro alla home page"> HOME PAGE</a>
     </li>
 </ul>
 </div>
