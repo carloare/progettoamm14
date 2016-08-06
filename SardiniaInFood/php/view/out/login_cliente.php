32ca6d38b6d6a6add70ceb82f0b471d11bed6d02
carloare <carlo.are@tiscali.it>
2016-08-06 14:49:16 +0200

    Update login_cliente.php

--- SardiniaInFood/php/view/out/login_cliente.php
+++ SardiniaInFood/php/view/out/login_cliente.php
@@ -8,7 +8,7 @@
       Gregorio Samsa, svegliandosi una mattina da sogni agitati, si trovò trasformato, nel suo letto, in un enorme insetto immondo. Riposava sulla schiena, dura come una corazza, e sollevando un poco il capo vedeva il suo ventre arcuato, bruno e diviso in tanti segmenti ricurvi, in cima a cui la coperta da letto, vicina a scivolar giù tutta, si manteneva a fatica. 
    </p>
    <div class="form-generic">
-      <form name="login_form" action="/SardiniaInFood/php/controller/BaseController.php" method="POST"> 
+      <form name="login_form" action="http://spano.sc.unica.it/amm2014/aresuCarlo/SardiniaInFood/php/controller/BaseController.php" method="POST"> 
          <input type="text" name="username_cliente" value="Username" title="inserisci il tuo username" onfocus="this.value=''" class="submitlogin"> 
          <input type="password" name="password_cliente" value="Password" title="inserisci la tua password" onfocus="this.value=''" class="submitlogin" ><br>
          <input type="hidden" name="cmd" value="login_cliente">
