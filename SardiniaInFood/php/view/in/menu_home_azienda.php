b664c84cf51710c88c73a076083432c3f0ef8896
carloare <carlo.are@tiscali.it>
2016-08-06 16:46:08 +0200

    Update menu_home_azienda.php

--- SardiniaInFood/php/view/in/menu_home_azienda.php
+++ SardiniaInFood/php/view/in/menu_home_azienda.php
@@ -4,16 +4,16 @@
               <a href="#">MODIFICA PROFILO</a>
               <div class="mobnav-subarrow"></div>
 	      <ul>
-                <li><a href="/SardiniaInFood/php/controller/AziendaController.php?cmd=modifica_profilo_personale">MODIFICA PROFILO PERSONALE</a></li>
-                <li><a href="/SardiniaInFood/php/controller/AziendaController.php?cmd=modifica_profilo_azienda">MODIFICA PROFILO AZIENDA</a></li>
-                <li><a href="/SardiniaInFood/php/controller/AziendaController.php?cmd=modifica_servizi">MODIFICA SERVIZI OFFERTI</a></li>
+                <li><a href="http://spano.sc.unica.it/amm2014/aresuCarlo/SardiniaInFood/php/controller/AziendaController.php?cmd=modifica_profilo_personale">MODIFICA PROFILO PERSONALE</a></li>
+                <li><a href="http://spano.sc.unica.it/amm2014/aresuCarlo/SardiniaInFood/php/controller/AziendaController.php?cmd=modifica_profilo_azienda">MODIFICA PROFILO AZIENDA</a></li>
+                <li><a href="http://spano.sc.unica.it/amm2014/aresuCarlo/SardiniaInFood/php/controller/AziendaController.php?cmd=modifica_servizi">MODIFICA SERVIZI OFFERTI</a></li>
               </ul>
             </li>
             <li>
-                <a class="alert" href ="/SardiniaInFood/php/controller/AziendaController.php?cmd=cancella" title="cancella il profilo di questa azienda" onclick ="return confirm('Sei sicuro?');">CANCELLA QUESTO PROFILO</a>
+                <a class="alert" href ="http://spano.sc.unica.it/amm2014/aresuCarlo/SardiniaInFood/php/controller/AziendaController.php?cmd=cancella" title="cancella il profilo di questa azienda" onclick ="return confirm('Sei sicuro?');">CANCELLA QUESTO PROFILO</a>
             </li> 
             <li class="logout"> 
-                <a href="http://localhost/SardiniaInFood/php/index.php?page=0&logout" method="POST" title="Esci">LOGOUT</a>
+                <a href="http://spano.sc.unica.it/amm2014/aresuCarlo/SardiniaInFood/php/index.php?page=0&logout" method="POST" title="Esci">LOGOUT</a>
 
             </li> 
           
