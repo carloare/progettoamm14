f1fba6810b4005972377b2bb958fa3d45a2f6917
carloare <carlo.are@tiscali.it>
2016-08-06 17:11:11 +0200

    Update customize-amm.js

--- SardiniaInFood/js/customize-amm.js
+++ SardiniaInFood/js/customize-amm.js
@@ -2,5 +2,5 @@ $(document).ready(function() {
     //colore header
     $("#header").css('background-color', '#29487d');
 
-    $("#mobnav-btn").css('background', '#12273a url(../images/menu-responsive.png) no-repeat right top');
+    $("#mobnav-btn").css('background', '#12273a url(http://spano.sc.unica.it/amm2014/aresuCarlo/SardiniaInFood/images/menu-responsive.png) no-repeat right top');
 });
