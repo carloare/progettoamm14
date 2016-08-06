e099cfcfc38c5943fc664ccd0fecc55acf833cc0
carloare <carlo.are@tiscali.it>
2016-08-06 12:42:36 +0200

    Update logo.php

--- SardiniaInFood/php/view/out/logo.php
+++ SardiniaInFood/php/view/out/logo.php
@@ -1,1 +1,1 @@
-<img src="http://spano.sc.unica.it/amm2014/aresuCarlo/SardiniaInFood/images/logo.png" title="SardiniaInFood" alt="SardiniaInFood logo">
+<img src="/home/amm/repoAmm/amm2014/aresuCarlo/SardiniaInFood/images/logo.png" title="SardiniaInFood" alt="SardiniaInFood logo">
