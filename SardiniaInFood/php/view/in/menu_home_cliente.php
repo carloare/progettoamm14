4f7624d27da2748273f516098c7385e2d00f5bff
carloare <carlo.are@tiscali.it>
2016-08-06 14:57:42 +0200

    Update menu_home_cliente.php

--- SardiniaInFood/php/view/in/menu_home_cliente.php
+++ SardiniaInFood/php/view/in/menu_home_cliente.php
@@ -4,7 +4,7 @@ include_once '/home/amm/development/SardiniaInFood/php/model/UtenteFactory.php';
 
 
         <li>     
-             <a href="/SardiniaInFood/php/controller/ClienteController.php?cmd=show_preferiti" title="mostra i miei preferiti" method="POST">PREFERITI</a>
+             <a href="http://spano.sc.unica.it/amm2014/aresuCarlo/SardiniaInFood/php/controller/ClienteController.php?cmd=show_preferiti" title="mostra i miei preferiti" method="POST">PREFERITI</a>
         </li>
         <?php
         if($_REQUEST['cmd']=='cercadovecosa')
@@ -22,14 +22,14 @@ echo '
 
 ?>
       <li>
-        <a class="alert" href ="/SardiniaInFood/php/controller/ClienteController.php?cmd=cancella" title="cancella il mio profilo" onclick ="return confirm('Sei sicuro?');">CANCELLA PROFILO</a>
+        <a class="alert" href ="http://spano.sc.unica.it/amm2014/aresuCarlo/SardiniaInFood/php/controller/ClienteController.php?cmd=cancella" title="cancella il mio profilo" onclick ="return confirm('Sei sicuro?');">CANCELLA PROFILO</a>
     </li>
     
 
     
     <li class="logout">
  
-        <a href="http://localhost/SardiniaInFood/php/index.php?page=0&logout" method="POST" title="Esci">LOGOUT</a>
+        <a href="http://spano.sc.unica.it/amm2014/aresuCarlo/SardiniaInFood/php/index.php?page=0&logout" method="POST" title="Esci">LOGOUT</a>
 
     </li> 
     
