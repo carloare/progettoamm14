f254ff30040a3f2cc7b2470459bdf33849cad154
carloare <carlo.are@tiscali.it>
2016-08-06 16:44:49 +0200

    Update menu_favorites_cliente.php

--- SardiniaInFood/php/view/in/menu_favorites_cliente.php
+++ SardiniaInFood/php/view/in/menu_favorites_cliente.php
@@ -1,7 +1,7 @@
 
   
          <li>     
-             <a href="/SardiniaInFood/php/controller/ClienteController.php?cmd=back_clear_home_page" title="torna alla tua home page" method="POST">HOME</a>
+             <a href="http://spano.sc.unica.it/amm2014/aresuCarlo/SardiniaInFood/php/controller/ClienteController.php?cmd=back_clear_home_page" title="torna alla tua home page" method="POST">HOME</a>
         </li>
          
     
