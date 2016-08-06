5bedb7adef2d69c87572355480a660fb04b85126
carloare <carlo.are@tiscali.it>
2016-08-06 16:29:01 +0200

    Update login_azienda.php

--- SardiniaInFood/php/view/out/login_azienda.php
+++ SardiniaInFood/php/view/out/login_azienda.php
@@ -1,5 +1,5 @@
 <!--form di login per l'azienda-->
-<script type="text/javascript" src="/SardiniaInFood/js/eliminasfondo.js"></script>
+<script type="text/javascript" src="http://spano.sc.unica.it/amm2014/aresuCarlo/SardiniaInFood/js/eliminasfondo.js"></script>
 <div id="box-form">
    <h1 class="white">
       Login Azienda
@@ -8,7 +8,7 @@
       Gregorio Samsa, svegliandosi una mattina da sogni agitati, si trovò trasformato, nel suo letto, in un enorme insetto immondo. Riposava sulla schiena, dura come una corazza, e sollevando un poco il capo vedeva il suo ventre arcuato, bruno e diviso in tanti segmenti ricurvi, in cima a cui la coperta da letto, vicina a scivolar giù tutta, si manteneva a fatica.
    </p>
    <div class="form-generic">
-      <form name="login_form" action="/SardiniaInFood/php/controller/BaseController.php" method="POST"> 
+      <form name="login_form" action="http://spano.sc.unica.it/amm2014/aresuCarlo/SardiniaInFood/php/controller/BaseController.php" method="POST"> 
          <input type="text" name="username_azienda" value="Username" title="inserisci il tuo username" onfocus="this.value=''" class="submitlogin"> 
          <input type="password" name="password_azienda" value="Password" title="inserisci la tua password" onfocus="this.value=''" class="submitlogin" ><br>
          <input type="hidden" name="cmd" value="login_azienda">
