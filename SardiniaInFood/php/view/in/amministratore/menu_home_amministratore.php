4843c410e52c6134e05b8bb5aeb068ea361761f8
carloare <carlo.are@tiscali.it>
2016-08-06 16:11:28 +0200

    Update menu_home_amministratore.php

--- SardiniaInFood/php/view/in/amministratore/menu_home_amministratore.php
+++ SardiniaInFood/php/view/in/amministratore/menu_home_amministratore.php
@@ -1,12 +1,12 @@
 
         <li class="blue">
  
-        <a href="/SardiniaInFood/php/controller/AmministratoreController.php?cmd=mod" title="moderazione clienti" method="POST">MODERAZIONE</a>
+        <a href="http://spano.sc.unica.it/amm2014/aresuCarlo/SardiniaInFood/php/controller/AmministratoreController.php?cmd=mod" title="moderazione clienti" method="POST">MODERAZIONE</a>
 
     </li> 
     <li class="blue">
  
-        <a href="http://localhost/SardiniaInFood/php/index.php?page=0&logout" method="POST" title="Esci">LOGOUT</a>
+        <a href="http://spano.sc.unica.it/amm2014/aresuCarlo/SardiniaInFood/php/index.php?page=0&logout" method="POST" title="Esci">LOGOUT</a>
 
     </li> 
    
