9bea2895d6534efba81cf2212ce6a860ef886700
carloare <carlo.are@tiscali.it>
2016-09-08 00:00:31 +0200

    Update form_registrazione_cliente.php

--- SardiniaInFood/php/view/out/form_registrazione_cliente.php
+++ SardiniaInFood/php/view/out/form_registrazione_cliente.php
@@ -4,9 +4,9 @@
    <h1 class="white">
       Registrazione nuovo cliente
    </h1>
-   <h3 class="center white">
+   <p class="center white">
       Inserisci i tuoi dati personali. 
-   </h3>
+   </p>
    <div class="form-generic">
       <form action="http://spano.sc.unica.it/amm2014/aresuCarlo/SardiniaInFood/php/controller/BaseController.php" id="registrazione_cliente" method="POST">
          <p class="submitrec"><label for="nome_completo_cliente">Nome Completo</label></p>
